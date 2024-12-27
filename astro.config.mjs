d1999160af57fce9aaee170ddaee90d7ed704484
TakiMoysha <mpwema782@gmail.com>
2024-12-27 23:53:42 +0100

    chore:update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { rehypeHeadingIds } from '@astrojs/markdown-remark';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://takimoysha.github.io",
   base: "/",
+  markdown: {
+    rehypePlugins: [
+      rehypeHeadingIds,
+
+    ]
+  },
 });
---
+++ src/site-config.ts
@@ -0,0 +1,14 @@
+export const siteConfig = {
+  author: "TakiMoysha",
+  title: "Moysha's Public Docs",
+  socialLinks: [
+    {
+      text: "GitHub",
+      href: "https://github.com/TakiMoysha",
+      icon: "i-simple-icons-gihtub",
+      header: "i-ri-gihtub-line",
+    },
+  ]
+};
+
+export default siteConfig;
